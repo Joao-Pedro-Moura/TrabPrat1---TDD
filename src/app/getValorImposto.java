004b8f922a320f875281f340fcb1ac03fbd84e61
João Pedro Moura <69908186+Joao-Pedro-Moura@users.noreply.github.com>
2022-04-25 17:53:56 -0300

    Refactoring: Extrair constante
    Refactoring: Extrair constante
    Mau-cheiro identificado: números "mágicos" espalhados pelo código.
    Descrição da refatoração: aplicou-se a refatoração para que se saiba o significado de cada número apresentado no código 
    Classes/métodos/atributos afetados: 
    * Classe getValorImposto teve adicionadas as constantes ALIQUOTA0, ALIQUOTA015, ALIQUOTA075, ALIQUOTA225, ALIQUOTA275 que representam os valores da alíquotas de cada faixa.
    * Classe getValorImposto teve adicionadas as constantes LIMITESUPERIOR4AFAIXA, LIMITESUPERIOR3AFAIXA, LIMITESUPERIOR2AFAIXA, LIMITESUPERIOR1AFAIXA que representam os valores máximos de cada faixa.
    * Classe getValorImposto teve adicionadas as constantes VALORMAXIMOIMPOSTO4AFAIXA, VALORMAXIMOIMPOSTO3AFAIXA que representam os valores máximos do imposto na quarta e terceira faixa, respectivamente.

--- src/app/getValorImposto.java
+++ src/app/getValorImposto.java
@@ -2,6 +2,20 @@ package app;
 
 public class getValorImposto {
 	
+	
+	//Valores das constantes
+	private static final float ALIQUOTA0 = 0f;
+	private static final double ALIQUOTA015 = 0.15f;
+	private static final double ALIQUOTA225 = 0.225f;
+	private static final double ALIQUOTA275 = 0.275f;
+	private static final double ALIQUOTA075 = 7.5/100f;
+	private static final double LIMITESUPERIOR4AFAIXA = 4664.68f;
+	private static final double LIMITESUPERIOR3AFAIXA = 3751.05f;
+	private static final double LIMITESUPERIOR2AFAIXA = 2826.65f;
+	private static final double LIMITESUPERIOR1AFAIXA = 1903.99f;
+	private static final double VALORMAXIMOIMPOSTO4AFAIXA = 205.5667f;
+	private static final double VALORMAXIMOIMPOSTO3AFAIXA = 138.66f;
+
 	//Refencia para objeto fonte
 	SimuladorIRPF _fonte;
 	
@@ -20,17 +34,17 @@ public class getValorImposto {
 	double computar() {
 		obterValorDaBaseDeCalculo();
 		inicializandoValorImposto();
-		inicializandoValoresDasFaixas();
+		inicializandoFaixasComValoresMaximos();
 		
-		if (valorBase < 1903.99) {
-			return 0f;
+		if (valorBase < LIMITESUPERIOR1AFAIXA) {
+			return ALIQUOTA0;
 		}
 		else {
-			if (valorBase > 2826.65) {
+			if (valorBase > LIMITESUPERIOR2AFAIXA) {
 				somarValorImpostoSegundaFaixaParaValorTotal();
-				if (valorBase > 3751.05) {
+				if (valorBase > LIMITESUPERIOR3AFAIXA) {
 					somarValorImpostoTerceiraFaixaParaValorTotal();
-					if (valorBase > 4664.68) {
+					if (valorBase > LIMITESUPERIOR4AFAIXA) {
 						obterValorImpostoAteQuintaFaixa(); 
 					}
 					else {
@@ -49,34 +63,34 @@ public class getValorImposto {
 	}
 
 	private void somarValorImpostoTerceiraFaixaParaValorTotal() {
-		imposto += 138.66;
+		imposto += VALORMAXIMOIMPOSTO3AFAIXA;
 	}
 
 	private void somarValorImpostoSegundaFaixaParaValorTotal() {
-		imposto = imposto + faixa2*(7.5/100);
+		imposto = imposto + faixa2*ALIQUOTA075;
 	}
 
 	private void obterValorImpostoAteQuintaFaixa() {
 		valorBase = valorBase - faixa1 - faixa2 - faixa3 - faixa4;
-		valorBase *= 0.275;
-		imposto = imposto + 205.5667 + valorBase;
+		valorBase *= ALIQUOTA275;
+		imposto = imposto + VALORMAXIMOIMPOSTO4AFAIXA + valorBase;
 	}
 
 	private void obterValorImpostoAteQuartaFaixa() {
 		faixa4 = valorBase - faixa1 - faixa2 - faixa3;
-		faixa4 *= 0.225;
+		faixa4 *= ALIQUOTA225;
 		imposto = imposto + faixa4;
 	}
 
 	private void obterValorImpostoAteSegundaFaixa() {
 		faixa2 -= faixa1;
-		faixa2 *= 0.075;
+		faixa2 *= ALIQUOTA075;
 		imposto = imposto + faixa2;
 	}
 
 	private void obterValorImpostoAteTerceiraFaixa() {
 		faixa3 = valorBase - faixa1 - faixa2;
-		faixa3 *= 0.15;
+		faixa3 *= ALIQUOTA015;
 		imposto = imposto + faixa3;
 	}
 
@@ -88,7 +102,7 @@ public class getValorImposto {
 		imposto = 0;
 	}
 
-	private void inicializandoValoresDasFaixas() {
+	private void inicializandoFaixasComValoresMaximos() {
 		faixa1 = 1903.98;
 		faixa2 = 922.67;
 		faixa3 = 924.40;
